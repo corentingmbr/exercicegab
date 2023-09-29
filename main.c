3d52c8d3c07e0492f84320f38c588f95f8554be5
Ryûga <corentingambier@gmail.com>
2023-09-29 10:25:39 +0200

    exogab

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct temps {
+    unsigned int heures;
+    unsigned int minutes;
+};
+
+void fonctionheure(struct temps instant) {
+    printf("Donnez l'heure : ");
+    scanf("%u", &instant.heures);
+
+    printf("Donnez les minutes : ");
+    scanf("%u", &instant.minutes);
+
+    if (instant.heures < 10) {
+        printf("il est :0%d", instant.heures);
+    } else {
+        printf("il est :%d", instant.heures);
+    }
+
+    printf(":");
+
+    if (instant.minutes < 10) {
+        printf("0%d", instant.minutes);
+    } else {
+        printf("%d", instant.minutes);
+    }
+
+    printf("\n");
+}
+
+int main() {
+    struct temps instant;
+    fonctionheure(instant);
+
+    return 0;
+}
